dfd91416c637e0b8caa30be378a912c64e6987f8
Sortomatt <mattsortomme120@gmail.com>
2017-04-05 09:43:37 -0600

    Final finale

--- acme/js/java.js
+++ acme/js/java.js
@@ -10,33 +10,40 @@
              var trap = data['nav-bar']['trap'];
              var output = '#flexlist';
 
-             $('#flexlist').html("<li><a href='#'>Home</a></li><li><a href='#'>Anvils</a></li><li><a href='#'>Explosives</a></li><li><a href='#'>Decoys</a></li><li><a href='#'>Traps</a></li>");
-
+             $('#flexlist').html("<li><a href='final.html'>Home</a></li><li><a href='#'>Anvils</a></li><li><a href='#'>Explosives</a></li><li><a href='#'>Decoys</a></li><li><a href='#'>Traps</a></li>");
 
 
              $("#flexlist").on("click", "a", function (evt) {
+
                  evt.preventDefault();
                  var navClick = $(this).text();
                  console.log(navClick);
+                 if (navClick != "Home") {
 
-                 var pname = data[navClick]['name'];
-                 var pdesc = data[navClick]['description'];
-                 var pmanu = data[navClick]['manufacturer'];
-                 var pprice = data[navClick]['price'];
-                 var preview = data[navClick]['reviews'];
-                 var pimage = data[navClick]['path']
-                 console.log(pmanu);
-                 $("#home").hide();
+                     var pname = data[navClick]['name'];
+                     var pdesc = data[navClick]['description'];
+                     var pmanu = data[navClick]['manufacturer'];
+                     var pprice = data[navClick]['price'];
+                     var preview = data[navClick]['reviews'];
+                     var pimage = data[navClick]['path']
+                     console.log(pmanu);
 
-
-                 $("#pname").html(pname);
-                 $("#pdesc").html(pdesc);
-                 $("#pmanu").html("Made by: " + pmanu);
-                 $("#pprice").html("Price: $" + pprice);
-                 $("#previews").html("Reviews: " + preview);
-                 $("#pimg").html(pimage);
-
-
+                     $("#page").removeClass("gone");
+                     $("#home").addClass("gone");
+                     
+                     var output = ('<img alt="Product Image" src="' + pimage + '">');
+                     $("#pname").html(pname);
+                     $("#pdesc").html(pdesc);
+                     $("#pmanu").html("Made by: " + pmanu);
+                     $("#pprice").html("Price: $" + pprice);
+                     $("#previews").html("Reviews: " + preview);
+                     $("#pimg").html(output);
+                     console.log(output);
+                 }
+                 else {
+                     $("#page").addClass("gone");
+                     $("#home").removeClass("gone");
+                 }
              });
          }
      });
--- code.js
+++ code.js
@@ -15,41 +15,86 @@
              $("#four").html(trap);
          }
      });
-             $("#page-nav").on("click", "a", function (evt) {
-                 evt.preventDefault();
-                 var navClick = $(this).text();
-                 console.log(navClick);
-                 var pname = data[navClick]['path'];
-                 var pdesc = data[navClick]['description'];
-                 var pmanu = data[navClick]['manufacturer'];
-                 var pprice = data[navClick]['price'];
-                 var preview = data[navClick]['review']
-                 console.log(pmanu);
-                 $("#pname").html(pname);
-                 $("pdesc").html(pdesc);
-                 $("pmanu").html(pmanu);
-                 $("pprice").html(pprice);
-                 $("previews").html(preview);
-                 $("#home").hide();
-             });
-//     $.ajax({
-//         url: "js/acme.json",
-//         dataType: "json",
-//         success: function (data) {
-//             console.log(data);
-//             var home = data['nav-bar']['home'];
-//             var anvil = data['nav-bar']['anvil'];
-//             var explosive = data['nav-bar']['tnt'];
-//             var decoy = data['nav-bar']['decoy'];
-//             var trap = data['nav-bar']['trap'];
-//             var output = '#flexlist';
-//
-//             $.getJSON("js/acme.json", function (data) {
-//                 $.each(data, function (key, val) {
-//                     output += '<li>';
-//                     output += '<a href="#"' + ' onclick="getData(' +
-//                         val.name + ')"' + ' title="go to ' + val.name + '">' + val.name + '</a>';
-//                     output += '</li>';
-//                     console.log(output);
-//                 });
-//             });
+     $("#flexlist").on("click", "a", function (evt) {
+
+         evt.preventDefault();
+         var navClick = $(this).text();
+         console.log(navClick);
+
+         var pname = data[navClick]['name'];
+         var pdesc = data[navClick]['description'];
+         var pmanu = data[navClick]['manufacturer'];
+         var pprice = data[navClick]['price'];
+         var preview = data[navClick]['reviews'];
+         var pimage = data[navClick]['path']
+         console.log(pmanu);
+         $("#home").hide();
+
+         var output = ('<img alt="Product Image" src="' + pimage + '">');
+         $("#pname").html(pname);
+         $("#pdesc").html(pdesc);
+         $("#pmanu").html("Made by: " + pmanu);
+         $("#pprice").html("Price: $" + pprice);
+         $("#previews").html("Reviews: " + preview);
+         $("#pimg").html(output);
+         console.log(output);
+         //                 if (navClick = home) {
+         //                     $("#page").addClass(".gone");
+         //                     $("#home").removeClass(".gone");
+         //                 } else {
+         //                     $("#page").removeClass(".gone");
+         //                     $("#home").addClass(".gone");
+         //                 }
+     });
+     $("#flexlist").on("click", "a", function (evt) {
+
+         evt.preventDefault();
+         var navClick = $(this).text();
+         console.log(navClick);
+         if (navClick != home) {
+             var pname = data[navClick]['name'];
+             var pdesc = data[navClick]['description'];
+             var pmanu = data[navClick]['manufacturer'];
+             var pprice = data[navClick]['price'];
+             var preview = data[navClick]['reviews'];
+             var pimage = data[navClick]['path']
+             console.log(pmanu);
+
+             $("#page").removeClass(".gone");
+             $("#home").addClass(".gone");
+
+             var output = ('<img alt="Product Image" src="' + pimage + '">');
+             $("#pname").html(pname);
+             $("#pdesc").html(pdesc);
+             $("#pmanu").html("Made by: " + pmanu);
+             $("#pprice").html("Price: $" + pprice);
+             $("#previews").html("Reviews: " + preview);
+             $("#pimg").html(output);
+             console.log(output);
+         } else {
+             $("#page").removeClass(".gone");
+             $("#home").addClass(".gone");
+         }
+     });
+
+     $.ajax({
+                 url: "js/acme.json",
+                 dataType: "json",
+                 success: function (data) {
+                         console.log(data);
+                         var home = data['nav-bar']['home'];
+                         var anvil = data['nav-bar']['anvil'];
+                         var explosive = data['nav-bar']['tnt'];
+                         var decoy = data['nav-bar']['decoy'];
+                         var trap = data['nav-bar']['trap'];
+                         var output = '#flexlist';
+
+                         $.getJSON("js/acme.json", function (data) {
+                             $.each(data, function (key, val) {
+                                 output += '<li>';
+                                 output += '<a href="#"' + ' onclick="getData(' +
+                                     val.name + ')"' + ' title="go to ' + val.name + '">' + val.name + '</a>';
+                                 output += '</li>';
+                                 console.log(output);
+                             });
+                         });
